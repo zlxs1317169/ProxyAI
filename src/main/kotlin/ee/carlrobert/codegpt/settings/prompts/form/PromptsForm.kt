408314d1e23f22fd85ad6d3a644083cf7b2ab04f
Mirko Jantschke <mirko.jantschke@gmx.de>
2025-03-24 02:07:44 +0100

    fix: use correct array indices (#933)
    Co-authored-by: Mirko Jantschke <mjantschke@novomind.com>

--- src/main/kotlin/ee/carlrobert/codegpt/settings/prompts/form/PromptsForm.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/settings/prompts/form/PromptsForm.kt
@@ -104,11 +104,11 @@ class PromptsForm {
 
         val coreActionsFormState = getFormState<CoreActionPromptDetails>(coreActionsNode)
         settings.coreActions.apply {
-            editCode = coreActionsFormState[1].toState()
-            fixCompileErrors = coreActionsFormState[2].toState()
-            generateCommitMessage = coreActionsFormState[3].toState()
-            generateNameLookups = coreActionsFormState[4].toState()
-            reviewChanges = coreActionsFormState[5].toState()
+            editCode = coreActionsFormState[0].toState()
+            fixCompileErrors = coreActionsFormState[1].toState()
+            generateCommitMessage = coreActionsFormState[2].toState()
+            generateNameLookups = coreActionsFormState[3].toState()
+            reviewChanges = coreActionsFormState[4].toState()
         }
         settings.chatActions.prompts = getFormState<ChatActionPromptDetails>(chatActionsNode)
             .map { it.toState() }
