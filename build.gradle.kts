7a5598eb1d293028e439cac5138238d0a0c02a33
Aleksandr <aaudin90@yandex.ru>
2025-04-07 02:49:18 +0300

    Add local.properties to gitignore and register runCustomIde task using customIdePath property. (#973)
    Co-authored-by: a.iudin <a.iudin@vk.team>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,24 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.tasks.RunIdeTask
 import java.io.FileInputStream
 import java.util.*
 
+val localPropertiesFile = file("local.properties")
 val env = environment("env").getOrNull()
 
 fun loadProperties(filename: String): Properties = Properties().apply {
     load(FileInputStream(filename))
 }
+
+val localProperties: Properties? = if (localPropertiesFile.exists()) {
+  loadProperties("local.properties")
+} else {
+  null
+}
+
+val customIdePath: String? = localProperties?.getProperty("customIdePath")
 
 fun properties(key: String): Provider<String> {
   if ("win-arm64" == env) {
@@ -77,6 +87,29 @@ dependencies {
 tasks.register<Exec>("updateSubmodules") {
   workingDir(rootDir)
   commandLine("git", "submodule", "update", "--init", "--recursive")
+}
+
+/**
+ * Task to run a custom IntelliJ IDEA sandbox.
+ *
+ * This task launches a custom IntelliJ IDEA installation using the path specified in the
+ * 'customIdePath' property from local.properties.
+ *
+ * IMPORTANT:
+ * - On macOS, the path must include the 'Contents' directory (e.g., /Applications/IntelliJ IDEA.app/Contents).
+ * - For Windows or Linux, specify the appropriate path to the IntelliJ IDEA installation.
+ *
+ * Usage:
+ *   ./gradlew runCustomIde
+ */
+if (customIdePath != null) {
+    tasks.register<RunIdeTask>("runCustomIde") {
+        group = "intellij"
+        description = "Start custom idea sandbox"
+        ideDir.set(file(customIdePath))
+        environment("ENVIRONMENT", "LOCAL")
+        autoReloadPlugins.set(false)
+    }
 }
 
 tasks {
